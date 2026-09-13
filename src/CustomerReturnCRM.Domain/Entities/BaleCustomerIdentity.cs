808ed9ada70b6f240e80f4d67e42671d1f2478fb
mohamadrahmani <43249356+mohamadrahmani@users.noreply.github.com>
2026-09-13 23:26:04 +0330

    fix: keep EF mapping out of domain layer

--- src/CustomerReturnCRM.Domain/Entities/BaleCustomerIdentity.cs
+++ src/CustomerReturnCRM.Domain/Entities/BaleCustomerIdentity.cs
@@ -1,10 +1,7 @@
 using CustomerReturnCRM.Domain.Common;
-using Microsoft.EntityFrameworkCore;
 
 namespace CustomerReturnCRM.Domain.Entities;
 
-[Index(nameof(BusinessId), nameof(CustomerId), IsUnique = true)]
-[Index(nameof(BusinessId), nameof(BaleUserId), IsUnique = true)]
 public sealed class BaleCustomerIdentity : AuditableEntity
 {
     public Guid BusinessId { get; set; }
