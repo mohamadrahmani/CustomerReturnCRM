a11d4d1bad848bd5c2b47556b53258f63139ed26
mohamadrahmani <43249356+mohamadrahmani@users.noreply.github.com>
2026-09-24 17:41:07 +0330

    refactor: prepare visit flow implementation

---
+++ src/CustomerReturnCRM.Application/VisitManagement/VisitManagementService.cs
@@ -0,0 +1,8 @@
+using CustomerReturnCRM.Domain.Entities;
+
+namespace CustomerReturnCRM.Application.VisitManagement;
+
+public class VisitManagementService
+{
+    // Visit/appointment flow changes are implemented in the API layer and domain services.
+}
